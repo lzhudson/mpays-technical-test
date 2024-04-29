b4727b2fe9dccb754cc4601db2b90106834e886d
Hudson Holanda <hudsonholanda55@gmail.com>
2024-04-29 14:05:24 -0300

    feat: create script from testimonials slider

---
+++ wp-content/themes/mpays/src/js/components/slider.js
@@ -0,0 +1,41 @@
+// eslint-disable-next-line import/no-extraneous-dependencies, no-unused-vars
+import slick from 'slick-carousel';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import $ from 'jquery';
+
+// eslint-disable-next-line func-names
+$(document).ready(function () {
+	$('.testimonials__slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					infinite: true,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					dots: true,
+				},
+			},
+		],
+	});
+});
