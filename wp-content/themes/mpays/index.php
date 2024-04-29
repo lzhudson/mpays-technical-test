fe59fd36703b8a51aa8630457d271f06af7df03e
Hudson Holanda <hudsonholanda55@gmail.com>
2024-04-29 13:03:43 -0300

    feat: create section create account

--- wp-content/themes/mpays/index.php
+++ wp-content/themes/mpays/index.php
@@ -1,2 +1,43 @@
 <?php get_header(); ?>
+<main>
+  <section class="create-account">
+    <div class="container">
+      <div class="create-account__content">
+        <div>
+          <h2 class="create-account__title">Criar sua conta</h2>
+          <p class="create-account__info">
+            Preencha o formulário ao lado para cadastrar-se no mpays.
+          </p>
+        </div>
+        <form class="create-account__form">
+          <div class="form-group">
+            <label class="form-label" for="full-name">
+              Nome e sobrenome *
+            </label>
+            <input class="form-input" id="full-name" type="text" placeholder="Nome">
+          </div>
+          <div class="form-group">
+            <label class="form-label" for="email">
+              E-mail *
+            </label>
+            <input class="form-input" id="email" type="email" placeholder="E-mail">
+          </div>
+          <div class="form-group">
+            <label class="form-label" for="phone">
+              Telefone *
+            </label>
+            <input class="form-input" id="phone" type="text" placeholder="Telefone">
+          </div>
+          <div class="form-group">
+            <label class="form-label" for="password">
+              Senha *
+            </label>
+            <input class="form-input" id="password" type="password" placeholder="⚉ ⚉ ⚉ ⚉ ⚉ ⚉ ⚉ ⚉">
+          </div>
+          <button class="create-account__form-submit" type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  </section>
+</main>
 <?php get_footer(); ?>
