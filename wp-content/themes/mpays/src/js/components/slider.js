8113d020526b31de15df060ec24059527fba7483
Hudson Holanda <hudsonholanda55@gmail.com>
2024-04-29 16:10:00 -0300

    feat: create new script from advantages slider

--- wp-content/themes/mpays/src/js/components/slider.js
+++ wp-content/themes/mpays/src/js/components/slider.js
@@ -39,4 +39,11 @@ $(document).ready(function () {
 			},
 		],
 	});
+	$('.advantages__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+		nextArrow: '<button type="button" class="slick-next">Next</button>',
+	});
 });
