f4ee1433c99fac8e55eb6f1cade7e85be29ae7fc
Hudson Holanda <hudsonholanda55@gmail.com>
2024-04-29 22:17:32 -0300

    feat: integrate Header

--- wp-content/themes/mpays/header.php
+++ wp-content/themes/mpays/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -7,9 +8,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap"
+    rel="stylesheet">
   <?php wp_head(); ?>
 </head>
+
 <body>
   <header class="header">
     <div class="container header__container">
@@ -23,23 +27,17 @@
         <button class="header__close-menu" title="Fechar menu">
           <img src="<?php echo PATH_TO_IMAGES . 'close-icon.svg';?>" alt="Ícone de fechar o menu">
         </button>
-        <ul class="menu">
-          <li class="menu__item">
-            <a class="menu__link" href="">Soluções</a>
-          </li>
-          <li class="menu__item">
-            <a class="menu__link" href="">Tarifas</a>
-          </li>
-          <li class="menu__item">
-            <a class="menu__link" href="">Depoimentos</a>
-          </li>
-          <li class="menu__item">
-            <a class="menu__link" href="">Contato</a>
-          </li>
-          <li class="menu__item">
-            <a class="menu__link" href="">Entrar</a>
-          </li>
-        </ul>
+        <?php
+          wp_nav_menu( array(
+            'theme_location' => 'main-menu',
+            'menu_id' => 'main-menu',
+            'menu_class' => 'menu',
+            'container' => false
+          ));
+        ?>
+        <a class="header__link" href="">
+          Entrar
+        </a>
       </nav>
       <a class="header__link-user" href="">
         <img src="<?php echo PATH_TO_IMAGES . 'user-icon.svg';?>" alt="Ícone de usuário">
