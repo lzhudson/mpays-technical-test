9a37250f28b435729a93f2f954eb85b863019948
Hudson Holanda <hudsonholanda55@gmail.com>
2024-04-30 09:17:12 -0300

    feat: add dots in slider

--- wp-content/themes/mpays/src/js/components/slider.js
+++ wp-content/themes/mpays/src/js/components/slider.js
@@ -43,6 +43,7 @@ $(document).ready(function () {
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		arrows: true,
+		dots: true,
 		prevArrow: '<button type="button" class="slick-prev">Previous</button>',
 		nextArrow: '<button type="button" class="slick-next">Next</button>',
 	});
