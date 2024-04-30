92f33b28df8df81f1d1563e2e86e1357738dd6eb
Hudson Holanda <hudsonholanda55@gmail.com>
2024-04-29 21:54:37 -0300

    feat: integrate advantages section

--- wp-content/themes/mpays/front-page.php
+++ wp-content/themes/mpays/front-page.php
@@ -18,72 +18,47 @@
   <section class="advantages">
     <div class="container">
       <div class="advantages__slider">
-        <div>
-          <div class="advantage-card">
-            <div class="advantage-card__content">
-              <h2 class="advantage-card__title">Pagamentos com máxima aprovação</h2>
-              <p class="advantage-card__info">
-                Venda online com a maior taxa de aprovação possível. Reduza o número de vendas recusadas, autorize o
-                maior
-                número de pagamentos, receba e gerencie seu dinheiro de forma flexível.
-              </p>
-              <a class="advantage-card__link" href="">Cadastre-se</a>
-            </div>
-            <div class="advantage-card__image-container">
-              <picture>
-                <source media="(min-width: 769px)" srcset="<?php echo PATH_TO_IMAGES . 'background-slider.png';?>">
-                <source media="(max-width: 768px)"
-                  srcset="<?php echo PATH_TO_IMAGES . 'background-slider-mobile.png';?>">
-                <img class="advantage-card__image" src="<?php echo PATH_TO_IMAGES . 'background-slider.png';?>"
-                  alt="Homem utilizando computador">
-              </picture>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div class="advantage-card">
-            <div class="advantage-card__content">
-              <h2 class="advantage-card__title">Pagamentos com máxima aprovação</h2>
-              <p class="advantage-card__info">
-                Venda online com a maior taxa de aprovação possível. Reduza o número de vendas recusadas, autorize o
-                maior
-                número de pagamentos, receba e gerencie seu dinheiro de forma flexível.
-              </p>
-              <a class="advantage-card__link" href="">Cadastre-se</a>
-            </div>
-            <div class="advantage-card__image-container">
-              <picture>
-                <source media="(min-width: 769px)" srcset="<?php echo PATH_TO_IMAGES . 'background-slider.png';?>">
-                <source media="(max-width: 768px)"
-                  srcset="<?php echo PATH_TO_IMAGES . 'background-slider-mobile.png';?>">
-                <img class="advantage-card__image" src="<?php echo PATH_TO_IMAGES . 'background-slider.png';?>"
-                  alt="Homem utilizando computador">
-              </picture>
+        <?php
+          $advantagesSectionGroup = get_field('advantages_section');
+          $advantages = $advantagesSectionGroup['advantages'];
+        ?>
+        <?php foreach($advantages as $advantage) : ?>
+          <?php
+            $advantageTitle = $advantage['title'];
+            $advantageInformation = $advantage['information'];
+            $advantageLink = $advantage['link'];
+            $advantageLinkText = $advantageLink['text'];
+            $advantageLinkUrl = $advantageLink['url'];
+            $advantageImages = $advantage['images'];
+            $advantageImageMobile = $advantageImages['image_mobile'];
+            $advantageImageDesktop = $advantageImages['image_desktop'];
+            var_dump($advantageImages);
+            $advantageMobileUrl = $advantageImageMobile['url'];
+            $advantageMobileAlt = $advantageImageMobile['alt'];
+            $advantageDesktopUrl = $advantageImageDesktop['url'];
+            $advantageDesktopAlt = $advantageImageDesktop['alt'];
+          ?>
+          <div>
+            <div class="advantage-card">
+              <div class="advantage-card__content">
+                <h2 class="advantage-card__title"><?php echo $advantageTitle;?></h2>
+                <p class="advantage-card__info">
+                  <?php echo $advantageInformation;?>
+                </p>
+                <a class="advantage-card__link" href="<?php echo $advantageLinkUrl;?>"><?php echo $advantageLinkText;?></a>
+              </div>
+              <div class="advantage-card__image-container">
+                <picture>
+                  <source media="(min-width: 769px)" srcset="<?php echo $advantageDesktopUrl;?>">
+                  <source media="(max-width: 768px)"
+                    srcset="<?php echo $advantageMobileUrl;?>">
+                  <img class="advantage-card__image" src="<?php echo $advantageDesktopUrl;?>"
+                    alt="<?php echo $advantageDesktopAlt;?>">
+                </picture>
+              </div>
             </div>
           </div>
-        </div>
-        <div>
-          <div class="advantage-card">
-            <div class="advantage-card__content">
-              <h2 class="advantage-card__title">Pagamentos com máxima aprovação</h2>
-              <p class="advantage-card__info">
-                Venda online com a maior taxa de aprovação possível. Reduza o número de vendas recusadas, autorize o
-                maior
-                número de pagamentos, receba e gerencie seu dinheiro de forma flexível.
-              </p>
-              <a class="advantage-card__link" href="">Cadastre-se</a>
-            </div>
-            <div class="advantage-card__image-container">
-              <picture>
-                <source media="(min-width: 769px)" srcset="<?php echo PATH_TO_IMAGES . 'background-slider.png';?>">
-                <source media="(max-width: 768px)"
-                  srcset="<?php echo PATH_TO_IMAGES . 'background-slider-mobile.png';?>">
-                <img class="advantage-card__image" src="<?php echo PATH_TO_IMAGES . 'background-slider.png';?>"
-                  alt="Homem utilizando computador">
-              </picture>
-            </div>
-          </div>
-        </div>
+        <?php endforeach;?>
       </div>
     </div>
   </section>
